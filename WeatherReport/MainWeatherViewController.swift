b189152b0e5a1a022ea60c8a083763e0a4b6718a
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-08 21:40:22 +0900

    ColorAssets、TextFieldDelegate追加

--- WeatherReport/MainWeatherViewController.swift
+++ WeatherReport/MainWeatherViewController.swift
@@ -16,9 +16,46 @@ final class MainWeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
 }
 
+//MARK: - UITextFieldDelegate
+
+extension MainWeatherViewController: UITextFieldDelegate {
+    
+    // エンターキーを押した時
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    // 入力しないでエンターした時
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "都市名を入力してください"
+            return false
+        }
+    }
+    
+    // エンター(入力が終わった時)、TextFieldを空にする
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+//        if let city = searchTextField.text {
+//            weatherManager.fetchWeather(cityName: city)
+//        }
+        
+        searchTextField.text = ""
+        
+    }
+    
+}
